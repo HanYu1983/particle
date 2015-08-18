e6d475bec1693151752c1bdd5624278fb7b01e1f
HanYu <hanyushome@gmail.com>
2015-08-18 14:19:17 +0800

    no message

--- goapp/src/stock/js/api.js
+++ goapp/src/stock/js/api.js
@@ -1,6 +1,10 @@
 var api = {};
 
 (function(pkg){
+	
+	var cbid = 0
+	var cbs = {}
+	
 	common.onModel.subscribe(function(param){
 		var type = param[0]
 		var data = param[1]
@@ -15,9 +19,22 @@ var api = {};
 		console.log( data )
 	})
 
-	var cbid = 0
-	var cbs = {}
-
+	/**
+	畫k線
+	id: stock id
+	type: kline | volume | clock | null
+	offset: 起始偏移。2為從到數第2K剛始畫
+	count: k線的數量
+	sub: 
+		[{
+			t: ma | ema | kd | macd | yu
+			d: {
+				n: int,
+				m: int,
+				color: red | ...
+			}
+		}]
+	*/
 	function draw( canvas, id, type, offset, count, sub ){
 		sub = sub || {}
 		sub.canvas = canvas
@@ -27,7 +44,12 @@ var api = {};
 		sub.count = count
 		common.onView.onNext(["draw", sub])
 	}
-
+	
+	/**
+	讀取資料
+	id: stock id
+	reset: 是否取代舊的（還沒實做)
+	*/
 	function stockId( id, reset, cb ){
 		var params = {
 			id: id,
@@ -39,10 +61,17 @@ var api = {};
 		common.onView.onNext(["stockId", params])
 	}
 
+	/**
+	印出資料在console。開發者用
+	*/
 	function print(){
 		common.onView.onNext(["print"])
 	}
 	
+	
+	/**
+	讀取使用者資料
+	*/
 	function load( fbid, cb ){
 		var params = {
 			fbid: fbid,
@@ -52,6 +81,9 @@ var api = {};
 		common.onView.onNext(["load", params])
 	}
 	
+	/**
+	記錄使用者資料
+	*/
 	function save( fbid, data, cb ){
 		var params = {
 			fbid: fbid,
