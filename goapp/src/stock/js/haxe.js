67eec326ec2ac3fbf6253e39a96a80a20113742b
vic <finalsteve@hotmail.com>
2015-08-25 10:50:58 +0800

    update

--- goapp/src/stock/js/haxe.js
+++ goapp/src/stock/js/haxe.js
@@ -202,7 +202,7 @@ Main.createNewStock = function(id,props) {
 };
 Main.createNewLine = function(type,deletable,props) {
 	if(deletable == null) deletable = true;
-	return { id : Main.getId(), type : type, deletable : deletable, sub : Main.createProp(props == null?[["group","均線"],["ma",false,5,10,20,40],["ema",false,5,10,20,40],["bbi",false,3,6,12,24],["ebbi",false,6,18,54,162],["macd",false,12,26,9,0],["group","價量"],["AccDist",false,14,0,0,0],["Chaikin",false,3,10,1,0],["eom",false,14,1,9,0],["group","威爾德"],["osc",false,10,20,0,0],["rsi",false,14,9,0,0],["dmi",false,14,14,0,0],["sar",false,3,0,0,0],["group","余氏"],["yu-clock",false,20,20,0,0],["yu-macd",false,5,12,9,0],["yu-car",false,1,.025,.7,0],["yu-kd",false,9,0,0,0],["group","其它"],["kd",false,9,3,3,0],["atr",false,14,0,0,0],["cv",false,10,10,0,0],["cci",false,10,0,0,0]]:props)};
+	return { id : Main.getId(), type : type, deletable : deletable, sub : Main.createProp(props == null?app.config.index:props)};
 };
 Math.__name__ = true;
 var Reflect = function() { };
--- haxe_workspace/stock/src/Main.hx
+++ haxe_workspace/stock/src/Main.hx
@@ -22,7 +22,7 @@ class Main
 	
 	function new() {
 		slideMessage( '歡迎使用', '余氏k線圖幫您變成操盤達人!' );
-		
+	
 		panelView.config = {
 			mc_accordionContainer:j("#mc_accordionContainer" ),
 			tmpl_panel:j("#tmpl_panel"),
@@ -187,37 +187,7 @@ class Main
 			id:getId(),
 			type:type,
 			deletable:deletable,
-			sub:createProp( props == null ? [
-												['group', '均線'],
-												['ma', false, 5, 10, 20, 40 ],
-												['ema', false, 5, 10, 20, 40 ],
-												['bbi', false, 3, 6, 12, 24 ],
-												['ebbi', false, 6, 18, 54, 162 ],
-												['macd', false, 12, 26, 9, 0 ],
-												
-												['group', '價量'],
-												['AccDist', false, 14, 0, 0, 0 ],
-												['Chaikin', false, 3, 10, 1, 0 ],
-												['eom', false, 14, 1, 9, 0 ],
-												
-												['group', '威爾德'],
-												['osc', false, 10, 20, 0, 0 ],
-												['rsi', false, 14, 9, 0, 0 ],
-												['dmi', false, 14, 14, 0, 0 ],
-												['sar', false, 3, 0, 0, 0 ],
-												
-												['group', '余氏'],
-												['yu-clock', false, 20, 20, 0, 0 ],
-												['yu-macd', false, 5, 12, 9, 0 ],
-												['yu-car', false, 1, .025, .7, 0 ],
-												['yu-kd', false, 9, 0, 0, 0 ],
-												
-												['group', '其它'],
-												['kd', false, 9, 3, 3, 0 ],
-												['atr', false, 14, 0, 0, 0 ],
-												['cv', false, 10, 10, 0, 0 ],
-												['cci', false, 10, 0, 0, 0 ]
-											] : props )
+			sub:createProp( props == null ? untyped __js__('app.config.index' ) : props )
 		}
 	}
 }
