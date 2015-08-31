48a549c7dda071445cb3e18adad70d3db7f6aeed
HanYu <hanyushome@gmail.com>
2015-08-31 17:30:05 +0800

    no message

--- goapp/src/stock/js/config.js
+++ goapp/src/stock/js/config.js
@@ -5,39 +5,40 @@ app.config.about += '<div>第二行第二行第二行第二行第二行第二行
 app.config.about += '<div><span style="color:red;">第三行</span>hahah</div>';
 app.config.about += '<div><a href="//gamer.com.tw" target="_blank">第四行</a></div>';
 app.config.index = [
+						['yu-test', 'N日K線' , false, 5, 0, 0, 0 ],
+	
 						['group', '均線'],
-						['ma','name', false, 5, 10, 20, 40, '//wiki.mbalib.com/wiki/Sma' ],
-						['ema','name', false, 5, 10, 20, 40, '//wiki.mbalib.com/wiki/EMA' ],
-						['bbi','name', false, 3, 6, 12, 24, '//wiki.mbalib.com/wiki/多空指数' ],
-						['ebbi','name', false, 6, 18, 54, 162, '//wiki.mbalib.com/wiki/多空指数' ],
-						['macd','name', false, 12, 26, 9, 0, '//wiki.mbalib.com/wiki/MACD' ],
+						['ma','簡單平均', false, 5, 10, 20, 40, '//wiki.mbalib.com/wiki/Sma' ],
+						['ema','指數平均', false, 5, 10, 20, 40, '//wiki.mbalib.com/wiki/EMA' ],
+						['bbi','多空指標', false, 3, 6, 12, 24, '//wiki.mbalib.com/wiki/多空指数' ],
+						['ebbi','指數多空', false, 6, 18, 54, 162, '//wiki.mbalib.com/wiki/多空指数' ],
+						['macd','指數差離', false, 12, 26, 9, 0, '//wiki.mbalib.com/wiki/MACD' ],
 						
 						['group', '價量'],
-						['AccDist','name', false, 14, 0, 0, 0, '//wiki.mbalib.com/wiki/A/D' ],
-						['Chaikin','name', false, 3, 10, 1, 0, '//wiki.mbalib.com/wiki/CHAIKIN' ],
-						['eom','name', false, 14, 1, 9, 0, '//wiki.mbalib.com/wiki/简易波动指标' ],
+						['AccDist','累積派發', false, 14, 0, 0, 0, '//wiki.mbalib.com/wiki/A/D' ],
+						['Chaikin','蔡金指標', false, 3, 10, 1, 0, '//wiki.mbalib.com/wiki/CHAIKIN' ],
+						['eom','簡易波動', false, 14, 1, 9, 0, '//wiki.mbalib.com/wiki/简易波动指标' ],
 						
 						['group', '威爾德'],
-						['osc', 'name' , false, 10, 20, 0, 0, '//wiki.mbalib.com/wiki/OSC' ],
-						['rsi', 'name' , false, 14, 9, 0, 0, '//wiki.mbalib.com/wiki/RSI' ],
-						['dmi', 'name' , false, 14, 14, 0, 0, '//wiki.mbalib.com/wiki/动向指数' ],
-						['sar', 'name' , false, 3, 0, 0, 0, '//wiki.mbalib.com/wiki/抛物线转向' ],
+						['osc', '振蕩量' , false, 10, 20, 0, 0, '//wiki.mbalib.com/wiki/OSC' ],
+						['rsi', '相對強弱' , false, 14, 9, 0, 0, '//wiki.mbalib.com/wiki/RSI' ],
+						['dmi', '趨向指標' , false, 14, 14, 0, 0, '//wiki.mbalib.com/wiki/动向指数' ],
+						['sar', '拋物線' , false, 3, 0, 0, 0, '//wiki.mbalib.com/wiki/抛物线转向' ],
 						
 						['group', '其它'],
-						['kd', 'name' , false, 9, 3, 3, 0, '//wiki.mbalib.com/wiki/随机指标' ],
-						['atr', 'name' , false, 14, 0, 0, 0, '//wiki.mbalib.com/zh-tw/真实波幅' ],
-						['cv', 'name' , false, 10, 10, 0, 0, '//wiki.mbalib.com/zh-tw/佳庆离散指标' ],
-						['cci', 'name' , false, 10, 0, 0, 0, '//wiki.mbalib.com/wiki/顺势指标' ],
-						['dpo', 'name' , false, 20, 9, 0, 0, '//wiki.mbalib.com/wiki/区间震荡线' ],
-						['trix', 'name' , false, 12, 9, 0, 0, '//wiki.mbalib.com/wiki/TRIX指标' ],
-						['uos', 'name' , false, 7, 14, 28, 9, '//www.taindicators.com/2010/04/ultimate-ultimate-oscillator.html' ],
+						['kd', '隨機指標' , false, 9, 3, 3, 0, '//wiki.mbalib.com/wiki/随机指标' ],
+						['atr', '真實波幅' , false, 14, 0, 0, 0, '//wiki.mbalib.com/zh-tw/真实波幅' ],
+						['cv', '蔡金離散' , false, 10, 10, 0, 0, '//wiki.mbalib.com/zh-tw/佳庆离散指标' ],
+						['cci', '順勢指標' , false, 10, 0, 0, 0, '//wiki.mbalib.com/wiki/顺势指标' ],
+						['dpo', '區間震蕩' , false, 20, 9, 0, 0, '//wiki.mbalib.com/wiki/区间震荡线' ],
+						['trix', '三重指數' , false, 12, 9, 0, 0, '//wiki.mbalib.com/wiki/TRIX指标' ],
+						['uos', '終極指標' , false, 7, 14, 28, 9, '//www.taindicators.com/2010/04/ultimate-ultimate-oscillator.html' ],
 	
 						['group', '余氏'],
-						['yu-clock', 'name' , false, 20, 20, 0, 0 ],
-						['yu-macd', 'name' , false, 5, 12, 9, 0 ],
-						['yu-car', 'name' , false, 1, .025, .7, 0 ],
-						['yu-kd', 'name' , false, 9, 0, 0, 0 ],
-						['yu-test', 'name' , false, 1, 1, 0, 0 ],
+						['yu-clock', '背離線' , false, 20, 20, 0, 0 ],
+						['yu-macd', '均多差離' , false, 5, 12, 9, 0 ],
+						['yu-car', '方向盤' , false, 1, .025, .7, 0 ],
+						['yu-kd', '隨機線圖' , false, 9, 0, 0, 0 ],
 					];
 app.config.hoverInfo = {
 	default:'',
@@ -48,7 +49,10 @@ app.config.hoverInfo = {
 	'btn_next10':'上十日<span style="color:yellow"> . </span>',
 	'btn_last':'返回最早<span style="color:yellow"> / </span>',
 	line:{
-		default:'點我學習!'
+		default:'點我學習!',
+		
+		'yu-car':'預測股價路線。基本上K線都會在上下軌之內，上下軌越大代表震蕩量越大－就有了投機空間；上下軌越小代表小紅小黑沒什振蕩，就順勢買賣就可以了',
+		'yu-kd':'將KD線的計算在K線上展開，方便配合其它指標',
 	}
 }
 app.config.preferStocks = [
