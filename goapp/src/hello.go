86240132b1909ddf0b2346725b53adb6918a9f85
HanYu <hanyushome@gmail.com>
2015-09-15 07:48:00 +0800

    no message

--- goapp/src/hello.go
+++ goapp/src/hello.go
@@ -9,8 +9,6 @@ import (
   auth "github.com/abbot/go-http-auth"
   "lib/game"
   "time"
-  "sync"
-  _ "sync/atomic"
 )
 
 func Secret(user, realm string) string {
@@ -21,77 +19,9 @@ func Secret(user, realm string) string {
   return ""
 }
 
-
-type TestC struct {
-  IDS []string
-}
-var ccc TestC
-var mutex sync.Mutex
-
-func WrapTestC (ccc *TestC) func(w http.ResponseWriter, r *http.Request){
-  return func (w http.ResponseWriter, r *http.Request){
-    ctx := appengine.NewContext( r )
-    r.ParseForm()
-    key := r.Form["key"][0]
-  
-    
-    ctx.Infof("enter!(%+v)(%v)", ccc, key)
-  
-    ch := make(chan TestC)
-    go func (){
-      mutex.Lock()
-      defer mutex.Unlock()
-      
-      ccc2 := *ccc
-      ccc2.IDS = append( ccc2.IDS, key )
-      time.Sleep(time.Second)
-      ccc2.IDS = append( ccc2.IDS, key )
-      ch <- ccc2
-      close( ch )
-    }()
-  
-    for ncc := range ch {
-      *ccc = ncc
-      ctx.Infof("XXXXX(%+v)(%v)", ccc, key)
-    }
-  
-    /*
-    var mutex sync.Mutex
-    var wg sync.WaitGroup
-    var _ = wg
-    ctx.Infof("enter!")
-    var _ = mutex
-    doA := func (){
-      mutex.Lock()
-      defer mutex.Unlock()
-      defer wg.Done()
-      //var ccc = acc.Load().(TestC)
-      ctx.Infof("(%+v)(%v)", ccc, key)
-      ccc.IDS = append( ccc.IDS, "0" )
-      time.Sleep(time.Second)
-      ctx.Infof("XXXXX(%+v)(%v)", ccc, key)
-      acc.Store( ccc )
-    }
-  
-    
-    go doA()
-    go doA()
-    wg.Add(1)
-    wg.Add(1)
-    wg.Wait()
-    */
-    ctx.Infof("done!")
-  }
-}
-
-
 func init(){
   authenticator := auth.NewDigestAuthenticator("dbpublic", Secret)
   dbfileHandler := authenticator.JustCheck(dbfile.DBFileSystem)
-  game.InitContext()
-  
-  var tc TestC
-  http.HandleFunc("/fn/test", WrapTestC(&tc))
   
   http.HandleFunc("/longPolling/", longPolling)
   http.HandleFunc("/", handler)
