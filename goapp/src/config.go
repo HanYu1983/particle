58e00362f0b57d6ad7a1105732b2bbca3fcb7cc4
vic <finalsteve@hotmail.com>
2015-09-16 23:39:44 +0800

    update

--- goapp/src/config.go
+++ goapp/src/config.go
@@ -2,10 +2,10 @@ package hello
 
 const (
   // han local
-  GameContextPosition = 6401356696911872
+  //GameContextPosition = 6401356696911872
   
   // vic local
-  //GameContextPosition = 6401356696911872
+  GameContextPosition = 6401356696911872
   
   // remote
   //GameContextPosition = 5668600916475904
