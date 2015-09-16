dfc5770dac9d11fb79131fe488d887069ca7bea2
HanYu <hanyushome@gmail.com>
2015-09-17 02:07:08 +0800

    no message

--- node_workspace/genCard/genGundamJson.js
+++ node_workspace/genCard/genGundamJson.js
@@ -1,20 +1,20 @@
 var fs = require('fs')
-var outputName = 'config.json'
+var outputName = 'gundamWar.json'
+var imageDir = 'gundamWar/'
 var targetDir = 'output/'
 var output = {
 	images: {}
 }
 fs.readdirSync(targetDir).forEach(function(name) {
-	var path = targetDir + name
+	var path = imageDir + name
 	output.images[name] = path
 });
 
-console.log(output)
-
 fs.writeFile(outputName, JSON.stringify(output), function(err) {
 	if(err) {
-		return console.log(err);
+		console.log(err);
+	} else {
+		console.log(outputName+" saved!");
 	}
-	console.log("The file was saved!");
 }); 
 
