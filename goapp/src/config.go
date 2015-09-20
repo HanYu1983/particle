42188943cc07265c43111669660f4d39edcb9b9e
HanYu <hanyushome@gmail.com>
2015-09-21 01:51:12 +0800

    no message

--- goapp/src/config.go
+++ goapp/src/config.go
@@ -3,15 +3,11 @@ package hello
 const (
   // han local
   //GameContextPosition = 6401356696911872
-  // vic local
-  //GameContextPosition = 6401356696911872
   // remote
   GameContextPosition = 5668600916475904
   
   // han local
-  CardInfoPosition = 5733953138851840
-  // vic local
   //CardInfoPosition = 5733953138851840
   // remote
-  //CardInfoPosition = 5733953138851840
+  CardInfoPosition = 5178081291534336
 )
