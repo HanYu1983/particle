32f170db1a9424c45e430ac058dc6faf9ae540f1
HanYu <hanyushome@gmail.com>
2015-09-24 19:13:32 +0800

    no message

---
+++ node_workspace/getMagicCard/genJson.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+var outputName = 'sangoWar.json'
+var imageDir = 'http://particle-979.appspot.com/common/cardPackage/sangoWar/'
+var targetDir = 'output/'
+var output = {
+	images: {}
+}
+fs.readdirSync(targetDir).forEach(function(name) {
+	var path = imageDir + name
+	output.images[name] = path
+});
+
+fs.writeFile(outputName, JSON.stringify(output), function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log(outputName+" saved!");
+	}
+}); 
+
