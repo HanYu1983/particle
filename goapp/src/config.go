9e75eb388e86ef177c7ec1cb180a11c1b50c24b7
HanYu <hanyushome@gmail.com>
2015-09-25 11:54:06 +0800

    no message

--- goapp/src/config.go
+++ goapp/src/config.go
@@ -2,11 +2,6 @@ package hello
 
 const (
   // han local
-  GameContextPosition = 6401356696911872
-  // remote
-  //GameContextPosition = 5668600916475904
-  
-  // han local
   CardInfoPosition = 5733953138851840
   // remote
   //CardInfoPosition = 5178081291534336
--- goapp/src/hello.go
+++ goapp/src/hello.go
@@ -5,11 +5,9 @@ import (
   "net/http"
   "lib/tool"
   "lib/db/file"
-  auth "lib/hack/go-http-auth"
-  "lib/game"
   "appengine"
   "appengine/user"
-  "appengine/channel"
+  auth "lib/hack/go-http-auth"
 )
 
 func Secret(user, realm string) string {
@@ -24,7 +22,6 @@ func init(){
   authenticator := auth.NewDigestAuthenticator("dbpublic", Secret)
   dbfileHandler := authenticator.JustCheck(dbfile.DBFileSystem)
   
-  game.InitContext( GameContextPosition )
   http.HandleFunc("/welcome/", welcome)
   http.HandleFunc("/", handler)
   http.HandleFunc("/proxy", tool.Proxy)
@@ -32,13 +29,6 @@ func init(){
   http.HandleFunc("/write", dbfile.WriteFile)
   http.HandleFunc("/simple/save", Save)
   http.HandleFunc("/simple/load", Load)
-  http.HandleFunc("/fn/card/user/", game.CreateUser)
-  http.HandleFunc("/fn/card/room/", game.CreateRoom)
-  http.HandleFunc("/fn/card/enterRoom/", game.EnterRoom)
-  http.HandleFunc("/fn/card/message/", game.LeaveMessage)
-  http.HandleFunc("/fn/card/clear", game.Clear)
-  http.HandleFunc("/fn/card/longPollingTargetMessage", game.LongPollingTargetMessage)
-  http.HandleFunc("/fn/card/state", game.State)
   http.HandleFunc("/fn/cardInfo/addCard", AddCard)
   http.HandleFunc("/fn/cardInfo/deleteCard", DeleteCard)
   http.HandleFunc("/fn/cardInfo/cardList", CardList)
@@ -55,73 +45,16 @@ func handler(w http.ResponseWriter, r *http.Request){
 
 func welcome(w http.ResponseWriter, r *http.Request) {
   c := appengine.NewContext(r)
-      u, err := user.CurrentOAuth(c, "")
-      if err != nil {
-          http.Error(w, "OAuth Authorization header required", http.StatusUnauthorized)
-          return
-      }
-      if !u.Admin {
-          http.Error(w, "Admin login only", http.StatusUnauthorized)
-          return
-      }
-      fmt.Fprintf(w, `Welcome, admin user %s!`, u)
-}
-
-func CreateChannel(w http.ResponseWriter, r *http.Request) {
-  defer tool.Recover( func(err error){
-    Output( w, nil, err.Error() )
-  })
-  ctx := appengine.NewContext( r )
-  
-  r.ParseForm()
-  tool.Assert( tool.ParameterIsNotExist( r.Form, "Name" ) ) 
-  
-  name := r.Form["Name"][0]
-  tok, err := channel.Create(ctx, name)
-  tool.Assert( tool.IfError( err ) ) 
-  
-  Output( w, tok, nil )
-}
-
-func SendChannelMessage(w http.ResponseWriter, r *http.Request) {
-  defer tool.Recover( func(err error){
-    Output( w, nil, err.Error() )
-  })
-  ctx := appengine.NewContext( r )
-  
-  form, err := tool.ReadAjaxPost( r )
-  tool.Assert( tool.IfError( err ) ) 
-
-  tool.Assert( tool.ParameterIsNotExist( form, "Name" ) ) 
-  tool.Assert( tool.ParameterIsNotExist( form, "Message" ) ) 
-  
-  name := form["Name"][0]
-  msg := form["Message"][0]
-  
-  err = channel.SendJSON(ctx, name, msg)
-  tool.Assert( tool.IfError( err ) ) 
-  
-  Output( w, nil, nil )
-}
-
-func onChannelConnected(w http.ResponseWriter, r *http.Request) {
-  /*
-  r.ParseForm()
-  from := r.FormValue("from")
-  ctx := appengine.NewContext( r )
-  
-  err := channel.SendJSON(ctx, from, 0)
+  u, err := user.CurrentOAuth(c, "")
   if err != nil {
-    ctx.Infof("sending error: %v", err)
+    http.Error(w, "OAuth Authorization header required", http.StatusUnauthorized)
+    return
   }
-  */
+  if !u.Admin {
+    http.Error(w, "Admin login only", http.StatusUnauthorized)
+    return
+  }
+  fmt.Fprintf(w, `Welcome, admin user %s!`, u)
 }
 
-func onChannelDisconnected(w http.ResponseWriter, r *http.Request) {
-  /*
-  r.ParseForm()
-  from := r.FormValue("from")
-  ctx := appengine.NewContext( r )
-  ctx.Infof("onChannelDisconnected %v", from)
-  */
-}
+
