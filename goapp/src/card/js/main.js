d566823194bfb109f7c5045d6760d42fb7642db8
vic <finalsteve@hotmail.com>
2015-09-26 17:12:24 +0800

    update

--- goapp/src/card/js/main.js
+++ goapp/src/card/js/main.js
@@ -279,7 +279,7 @@ _$List_ListIterator.prototype = {
 };
 var Main = function() {
 	var _g = this;
-	Main.j("#txt_id").textbox({ editable : true, onChange : function(nv,od) {
+	Main.j("#txt_id").textbox({ editable : false, onChange : function(nv,od) {
 		Main.playerId = nv;
 	}});
 	Main.j("#txt_opponent").textbox({ onChange : function(nv1,od1) {
@@ -581,7 +581,7 @@ Main.closeLoading = function() {
 };
 Main.handleResponse = function(cb) {
 	return function(err,ret) {
-		if(err != null) Main.alert(err); else cb(ret);
+		if(err != null) Main.alert("錯誤已經回報"); else cb(ret);
 	};
 };
 Main.main = function() {
@@ -1075,6 +1075,9 @@ mediator_Layer.prototype = $extend(org_puremvc_haxe_patterns_mediator_Mediator.p
 		org_puremvc_haxe_patterns_mediator_Mediator.prototype.onRegister.call(this);
 		this._body.keyup($bind(this,this.onBodyKeyUp));
 		this._body.mousemove($bind(this,this.onBodyMouseMove));
+		window.document.addEventListener("contextmenu",function(e) {
+			e.preventDefault();
+		},false);
 		this._body.mousedown($bind(this,this.onBodyMouseDown));
 		leo.utils.initRectSelect(function(ary) {
 			_g.sendNotification(mediator_Layer.on_select_cards,{ ary_select : ary});
@@ -1793,5 +1796,3 @@ model_Model.on_state_change = "on_state_change";
 model_Model.on_select_cards = "on_model_select_cards";
 Main.main();
 })(typeof console != "undefined" ? console : {log:function(){}});
-
-//# sourceMappingURL=main.js.map
