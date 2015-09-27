79a70c096dc8623a588d554d3347f4e2158ea2fe
vic <finalsteve@hotmail.com>
2015-09-27 17:14:50 +0800

    update

---
+++ haxe_workspace/card/gameplay/src/AutoJs.hx
@@ -0,0 +1,37 @@
+package;
+import haxe.macro.Context;
+import haxe.macro.Expr.Access;
+import haxe.macro.Expr.Field;
+
+/**
+ * ...
+ * @author vic
+ */
+class AutoJs
+{
+	/**
+	 * 
+	 * @param	config : ['console.log', 'app_doA']
+	 * @return
+	 */
+	macro static public function build( config:Array<String> ):Array<Field> {
+		var fs = Context.getBuildFields();
+		var pos = Context.currentPos();
+		
+		Lambda.foreach( config, function( str ) {
+			var funstr = StringTools.replace( str, '_', '.' );
+			fs.push( {
+				kind:FVar(TPath({name:'Dynamic', pack:[]}),macro untyped __js__( $v { funstr } ) ),
+				access:[Access.APublic, Access.AStatic],
+				doc:null,
+				name:str,
+				meta:[],
+				pos:pos
+			});
+			
+			return true;
+		});
+		
+		return fs;
+	}
+}
