58d65acd2ba93dbd6ac48da728848ceadd754427
HanYu <hanyushome@gmail.com>
2015-10-07 14:27:58 +0800

    no message

--- goapp/src/particle/js/api.js
+++ goapp/src/particle/js/api.js
@@ -2,106 +2,6 @@ var api = api || {};
 
 (function( pkg ){
 	
-	function drawer( elem ){	
-		var useWebgl = true
-		var w = elem.width()
-		var h = elem.height()
-		var scene = new THREE.Scene
-		var camera = new THREE.OrthographicCamera( w/-2, w/2, h/2, h/-2, -500, 1000 )
-		camera.position.set( w/2, -h/2, 200 )
-
-		var renderer = useWebgl ? new THREE.WebGLRenderer({antialias: true}) : new THREE.CanvasRenderer({antialias: true})
-	
-		renderer.setSize( w, h )
-	
-		$(renderer.domElement).appendTo( elem )
-	
-		var objs = {}
-	
-		function createObject( scene, key ){
-			var obj = objs[key]
-		
-			if( obj == null ){
-			
-				var material = new THREE.MeshBasicMaterial( { map:null, color: 0x33aa55 } );
-				// 加上transparent:true才會有png的透明效果, 也才有blending效果
-				material.transparent = true;
-			
-				obj = new THREE.Mesh(new THREE.PlaneBufferGeometry(1, 1), material)
-				objs[key] = obj
-			}
-		
-			return obj
-		}
-		
-		function addObject( obj ){
-			scene.add( obj )
-		}
-	
-		function removeObject( key ){
-			var obj = objs[key]
-			if( obj != null ){
-				scene.remove( obj )
-				//不必刪除，用來重用
-				//delete objs[key]
-			}
-		}
-	
-		var life = {}
-		function compareAndReset( nowlife, fn ){
-			for(var k in nowlife){
-				delete life[k]
-			}
-			for( var k in life ){
-				fn(k)
-			}
-			life = nowlife
-		}
-	
-		return function( ctx ){
-		
-			var nowlife = {}
-			var parts = ctx.parts
-			for( var idx in parts ){
-				var part = parts[idx]
-				var obj = createObject( scene, part.poolId )
-				if( ctx.textures[ part.tex ] ){
-					obj.material.map = ctx.textures[ part.tex ]
-				}
-				
-				var material = obj.material
-				material.blending = THREE[ 'CustomBlending' ];
-				switch( part.blending ){
-				case 'normal':
-					//var blendings = [ "NoBlending", "NormalBlending", "AdditiveBlending", "SubtractiveBlending", "MultiplyBlending", "AdditiveAlphaBlending" ];
-					material.blending = THREE[ 'NormalBlending' ];
-					break;
-				case 'add':
-					material.blending = THREE[ 'AdditiveBlending' ];
-					break;
-				default:
-					material.blending = THREE[ 'NoBlending' ];
-				}
-				
-				obj.material.color.setRGB( part.color[0], part.color[1], part.color[2] )
-				obj.material.opacity = part.color[3]
-				obj.position.set( part.pos[0], -part.pos[1], 0 )
-				obj.rotation.z = part.pos[2]
-				obj.scale.set( part.size[0], part.size[1], 1 )
-				
-				addObject( obj )
-				nowlife[part.poolId] = true
-			}
-			compareAndReset( nowlife, function(key){
-				removeObject(key)
-			})
-			var bgColor = (ctx.bgColor[0]*255<<16) | (ctx.bgColor[1]*255<<8) | (ctx.bgColor[2]*255)
-			renderer.setClearColor( bgColor, 1 );
-			renderer.render( scene, camera )
-		}
-	}
-	
-	
 	var pool = particle.pool( 2000 )
 	var first = particle.initParticle( pool.get(), { 
 		id: 'root',
@@ -164,7 +64,14 @@ var api = api || {};
 		var w = jdom.width()
 		var h = jdom.height()
 		var countElem = $('#count')
-		var draw = drawer(jdom)
+		
+		var useWebgl = true
+		var renderer = useWebgl ? new THREE.WebGLRenderer({antialias: true}) : new THREE.CanvasRenderer({antialias: true})
+		renderer.setSize( w, h )
+		$(renderer.domElement).appendTo( jdom )
+		var scene = new THREE.Scene
+		var draw = particleDrawer.three(w, h, scene, renderer)
+		
 		var last = new Date().getTime()
 		setTimeout( function(){
 			var now = new Date().getTime()
