f6b046ec8ded7a1dd311ccd5879440c98fb70691
vic <finalsteve@hotmail.com>
2015-10-13 16:19:59 +0800

    update

--- goapp/src/particle/js/list_haxe.js
+++ goapp/src/particle/js/list_haxe.js
@@ -1,154 +1,64 @@
 (function (console) { "use strict";
 var CallJs = function() { };
-CallJs.__name__ = true;
+var HxOverrides = function() { };
+HxOverrides.iter = function(a) {
+	return { cur : 0, arr : a, hasNext : function() {
+		return this.cur < this.arr.length;
+	}, next : function() {
+		return this.arr[this.cur++];
+	}};
+};
+var Lambda = function() { };
+Lambda.foreach = function(it,f) {
+	var $it0 = $iterator(it)();
+	while( $it0.hasNext() ) {
+		var x = $it0.next();
+		if(!f(x)) return false;
+	}
+	return true;
+};
 var Main = function() { };
-Main.__name__ = true;
 Main.main = function() {
 	CallJs.api_loadParticleList(0,999,Main.handleModel(function(ret) {
-		Main.ary_items = Reflect.fields(ret).map(function(fn) {
-			return Reflect.field(ret,fn);
-		});
-		Main.showList(Main.ary_items);
+		Main.ary_items = ret;
+		Main.initPagination(Main.ary_items.length);
+		Main.showList(Main.mc_listContainer,Main.getShowAry(Main.ary_items,Main.currPage,Main.pageSize));
 	}));
-	Main.pag_page.pagination({ onSelectPage : function(pageNumber,pageSize) {
-		haxe_Log.trace(pageNumber,{ fileName : "Main.hx", lineNumber : 34, className : "Main", methodName : "main", customParams : [pageSize]});
+};
+Main.initPagination = function(total) {
+	Main.pag_page.pagination({ total : total, onSelectPage : function(pageNumber,pageSize) {
+		Main.currPage = pageNumber - 1;
+		Main.showList(Main.mc_listContainer,Main.getShowAry(Main.ary_items,Main.currPage,pageSize));
 	}});
 };
-Main.showList = function(ary_item) {
+Main.getShowAry = function(ary_item,currPage,pageSize) {
+	var finalCount = (currPage + 1) * pageSize;
+	if(finalCount > ary_item.length) return ary_item.slice(currPage * pageSize,ary_item.length); else return ary_item.slice(currPage * pageSize,currPage * pageSize + pageSize);
+};
+Main.showList = function(mc_listContainer,ary_item) {
+	Lambda.foreach(ary_item,function(item) {
+		var img = $("<a><img></img></a>");
+		img.attr("href","index.html?key=" + item.key);
+		img.attr("target","_blank");
+		img.find("img").attr("src",item.img);
+		img.find("img").addClass("item");
+		img.find("img").addClass("hori");
+		mc_listContainer.append(img);
+		return true;
+	});
 };
 Main.handleModel = function(fn) {
 	return function(err,ret) {
-		if(err == null) fn(ret); else haxe_Log.trace(err,{ fileName : "Main.hx", lineNumber : 48, className : "Main", methodName : "handleModel"});
+		if(err == null) fn(ret); else console.log(err);
 	};
 };
-Math.__name__ = true;
-var Reflect = function() { };
-Reflect.__name__ = true;
-Reflect.field = function(o,field) {
-	try {
-		return o[field];
-	} catch( e ) {
-		return null;
-	}
-};
-Reflect.fields = function(o) {
-	var a = [];
-	if(o != null) {
-		var hasOwnProperty = Object.prototype.hasOwnProperty;
-		for( var f in o ) {
-		if(f != "__id__" && f != "hx__closures__" && hasOwnProperty.call(o,f)) a.push(f);
-		}
-	}
-	return a;
-};
-var haxe_Log = function() { };
-haxe_Log.__name__ = true;
-haxe_Log.trace = function(v,infos) {
-	js_Boot.__trace(v,infos);
-};
-var js_Boot = function() { };
-js_Boot.__name__ = true;
-js_Boot.__unhtml = function(s) {
-	return s.split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;");
-};
-js_Boot.__trace = function(v,i) {
-	var msg;
-	if(i != null) msg = i.fileName + ":" + i.lineNumber + ": "; else msg = "";
-	msg += js_Boot.__string_rec(v,"");
-	if(i != null && i.customParams != null) {
-		var _g = 0;
-		var _g1 = i.customParams;
-		while(_g < _g1.length) {
-			var v1 = _g1[_g];
-			++_g;
-			msg += "," + js_Boot.__string_rec(v1,"");
-		}
-	}
-	var d;
-	if(typeof(document) != "undefined" && (d = document.getElementById("haxe:trace")) != null) d.innerHTML += js_Boot.__unhtml(msg) + "<br/>"; else if(typeof console != "undefined" && console.log != null) console.log(msg);
-};
-js_Boot.__string_rec = function(o,s) {
-	if(o == null) return "null";
-	if(s.length >= 5) return "<...>";
-	var t = typeof(o);
-	if(t == "function" && (o.__name__ || o.__ename__)) t = "object";
-	switch(t) {
-	case "object":
-		if(o instanceof Array) {
-			if(o.__enum__) {
-				if(o.length == 2) return o[0];
-				var str2 = o[0] + "(";
-				s += "\t";
-				var _g1 = 2;
-				var _g = o.length;
-				while(_g1 < _g) {
-					var i1 = _g1++;
-					if(i1 != 2) str2 += "," + js_Boot.__string_rec(o[i1],s); else str2 += js_Boot.__string_rec(o[i1],s);
-				}
-				return str2 + ")";
-			}
-			var l = o.length;
-			var i;
-			var str1 = "[";
-			s += "\t";
-			var _g2 = 0;
-			while(_g2 < l) {
-				var i2 = _g2++;
-				str1 += (i2 > 0?",":"") + js_Boot.__string_rec(o[i2],s);
-			}
-			str1 += "]";
-			return str1;
-		}
-		var tostr;
-		try {
-			tostr = o.toString;
-		} catch( e ) {
-			return "???";
-		}
-		if(tostr != null && tostr != Object.toString && typeof(tostr) == "function") {
-			var s2 = o.toString();
-			if(s2 != "[object Object]") return s2;
-		}
-		var k = null;
-		var str = "{\n";
-		s += "\t";
-		var hasp = o.hasOwnProperty != null;
-		for( var k in o ) {
-		if(hasp && !o.hasOwnProperty(k)) {
-			continue;
-		}
-		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__" || k == "__properties__") {
-			continue;
-		}
-		if(str.length != 2) str += ", \n";
-		str += s + k + " : " + js_Boot.__string_rec(o[k],s);
-		}
-		s = s.substring(1);
-		str += "\n" + s + "}";
-		return str;
-	case "function":
-		return "<function>";
-	case "string":
-		return o;
-	default:
-		return String(o);
-	}
-};
-String.__name__ = true;
-Array.__name__ = true;
-if(Array.prototype.map == null) Array.prototype.map = function(f) {
-	var a = [];
-	var _g1 = 0;
-	var _g = this.length;
-	while(_g1 < _g) {
-		var i = _g1++;
-		a[i] = f(this[i]);
-	}
-	return a;
-};
+function $iterator(o) { if( o instanceof Array ) return function() { return HxOverrides.iter(o); }; return typeof(o.iterator) == 'function' ? $bind(o,o.iterator) : o.iterator; }
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
 CallJs.store_loadParticleList = store.loadParticleList;
 CallJs.api_loadParticleList = api.loadParticleList;
 Main.pag_page = $("#pag_page");
+Main.mc_listContainer = $("#mc_listContainer");
 Main.currPage = 0;
 Main.pageSize = 30;
 Main.main();
