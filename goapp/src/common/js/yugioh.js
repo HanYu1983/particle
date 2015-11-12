3d10afd1b4e2eec2ae63393cd52f411904427125
HanYu <hanyushome@gmail.com>
2015-11-13 01:01:11 +0800

    no message

--- goapp/src/common/js/yugioh.js
+++ goapp/src/common/js/yugioh.js
@@ -2,35 +2,17 @@ var yugioh = yugioh || {};
 
 (function( module ) {
 	
-	function loadCSV( path, cb ){
-		$.get(path)
-			.pipe( CSV.parse )
-			.done( function(rows) {
-				cb( null, rows )
-			})
-	}
-	
-	function processCSV( path, cb ){
-		loadCSV( path, function( err, rows ){
-			if( err ){
+	function load( path, cb ){
+		$.ajax({
+			url: path,
+			dataType: 'json',
+			success: function( data ){
+				console.log( data )
+			},
+			error: function( xhr, res, err ){
 				cb( err )
-			} else {
-				cb( null, rows )
 			}
 		})
-	}
-	
-	function load( path, cb ){
-		$.get(path)
-			.done( function(rows) {
-				
-				async.waterfall([
-					async.map.bind( null, rows.filelist, processCSV )
-				], function( err, ret ){
-					console.log( ret )
-					cb( err, ret )
-				})
-			})
 	}
 	
 	module.load = load
---
+++ goappres/src/hello.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+  "net/http"
+  "fmt"
+)
+
+func init(){
+  http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Hello, world!")
+}
