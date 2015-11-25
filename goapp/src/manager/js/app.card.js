337fd2d854c506f810bb188a0600f0ab8313726c
vic <finalsteve@hotmail.com>
2015-11-25 17:40:46 +0800

    update

--- goapp/src/manager/js/app.card.js
+++ goapp/src/manager/js/app.card.js
@@ -31,39 +31,23 @@ app.card = app.card || {};
 	}
 	
 	function addDeck( _model ){
+	
 		var dom = $("#tmpl_deck" ).tmpl( _model );
 		$('#mc_deckContainer').append( dom );
 		dom.find( '.easyui-combobox' ).combobox({
+			value: _model.game,
 			onSelect:(function( __model ){
 				return function( record ){
-					console.log( record.value );
-					console.log( __model );
-					//__model.game = record.value;
-					//cardSuit.editCardSuit( loadModel, __model );
-					editAndShow();
-				}
-			})( _model )
-		});
-		/*
-		dom.find( '.easyui-linkbutton' ).linkbutton({
-			group:_model.name,
-			onClick:(function( __model ){
-				return function(){
-					switch( this.id ){
-						case 'btn_army':__model.game = 'army';break;
-						case 'btn_fighter':__model.game = 'fighter';break;
-						case 'btn_magic':__model.game = 'magic';break;
-						case 'btn_gundamWar':__model.game = 'gundamWar';break;
-						case 'btn_sangoWar':__model.game = 'sangoWar';break;
-						case 'btn_yugioh':__model.game = 'yugioh';break;
-					}
+					__model.game = record.value;
 					cardSuit.editCardSuit( loadModel, __model );
-					editAndShow();
+					var tid = setTimeout( function(){
+						clearTimeout( tid );
+						editAndShow();
+					}, 10 );
 				}
 			})( _model )
 		});
-		*/
-		dom.find( '.easyui-textbox' ).textbox();
+		
 		dom.find( '.easyui-textbox' ).textbox({
 			onChange:(function( __model ){
 				return function( nv, ov ){
@@ -92,38 +76,14 @@ app.card = app.card || {};
 				}
 			})( _model )
 		});
-		
 		var cardstr = (function(){
 			var str = JSON.stringify( _model.cards );
 			str = str.substr( 1, str.length - 2 );
 			return str;
 		})();
-		dom.find( '.easyui-combobox' ).combobox('setValue', _model.game);
-		/*
-		switch( _model.game ){
-			case 'army':
-				dom.find( '#btn_army' ).linkbutton( 'select' );
-				break;
-			case 'fighter':
-				dom.find( '#btn_fighter' ).linkbutton( 'select' );
-				break;
-			case 'magic':
-				dom.find( '#btn_magic' ).linkbutton( 'select' );
-				break;
-			case 'gundamWar':
-				dom.find( '#btn_gundamWar' ).linkbutton( 'select' );
-				break;
-			case 'sangoWar':
-				dom.find( '#btn_sangoWar' ).linkbutton( 'select' );
-				break;
-			case 'yugioh':
-				dom.find( '#btn_yugioh' ).linkbutton( 'select' );
-		}
-		*/
 		dom.find( '#txt_cards' ).textbox( {
 			value:cardstr
 		});
-		
 		dom.find( '#btn_remove' ).linkbutton();
 		dom.find( '#btn_remove' ).click( function(){
 			cardSuit.removeCardSuit( loadModel, $(this).attr( 'group' ) );
