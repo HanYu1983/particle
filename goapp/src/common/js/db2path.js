fa73648c02768f83b7201e19e71ba182e673d2f9
HanYu <hanyushome@gmail.com>
2016-01-08 10:32:31 +0800

    no message

---
+++ goapp/src/common/js/db2path.js
@@ -0,0 +1,18 @@
+var db2path = db2path || {};
+
+(function(module){
+	
+	function sf(s, args){
+		return s.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
+			if (m == "{{") { return "{"; }
+			if (m == "}}") { return "}"; }
+			return args[n]
+		})
+	}
+	
+	var cardSuitJson = "root/user/{0}/cardSuit.json"
+	var userInfoJson = "root/user/{0}/userInfo.json"
+	
+	module.sf = sf
+	
+}) (db2path)
