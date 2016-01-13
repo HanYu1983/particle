6a0f71966f7c3d601b039a8251dcc9e85239922b
vic <finalsteve@hotmail.com>
2016-01-13 12:42:06 +0800

    update

--- goapp/src/deckWiki/js/main.js
+++ goapp/src/deckWiki/js/main.js
@@ -631,7 +631,7 @@ model_ModelController.prototype = $extend(org_puremvc_haxe_patterns_mediator_Med
 			}
 			if(checkHash) Helper.loadRead(function(err1,readData) {
 				if(readData != null) _g.ary_read = JSON.parse(readData);
-				_g.doSetData(data,true);
+				_g.doSetData(data);
 				_g.checkHashAndShow();
 			}); else _g.doSetData(data);
 		});
@@ -707,10 +707,9 @@ model_ModelController.prototype = $extend(org_puremvc_haxe_patterns_mediator_Med
 			onLoadSuccess(ary_showData1);
 		}
 	}
-	,doSetData: function(data,setRead) {
-		if(setRead == null) setRead = false;
+	,doSetData: function(data) {
 		this.oriDataToUseData(data);
-		if(setRead) this.setDataRead();
+		this.setDataRead();
 		this.sendNotification(view_ViewController.do_show_list,{ data : this.filterByPage(data,0), total : data.length, pageNumber : 1});
 	}
 	,setDataRead: function() {
