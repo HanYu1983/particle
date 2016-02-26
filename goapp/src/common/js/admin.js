caa4676c1bb7421342c203f42a3c6a0f43a0c2d2
vic <finalsteve@hotmail.com>
2016-02-26 14:19:00 +0800

    update

--- goapp/src/common/js/admin.js
+++ goapp/src/common/js/admin.js
@@ -63,7 +63,7 @@ var admin = admin || {};
 	}
 	
 	// ============== 熱門遊戲設定 ================ //
-	
+	/*
 	var ary_hotGame = ['sgs', 'sangoWar', 'battleSpirits','gundamWarN'];
 	function showHotGame( dom ){
 		dom.find( 'a' ).each( function( id, dom ){
@@ -76,16 +76,32 @@ var admin = admin || {};
 			}
 		});
 	}
+	*/
+	var ary_games = [
+		{ game:'army', name:'軍武gogogo', hot:false },
+		{ game:'gundamWar', name:'鋼彈大戰', hot:false },
+		{ game:'gundamWarN', name:'鋼彈大戰NexA', hot:true },
+		{ game:'sengoku', name:'戰國大戰', hot:false },
+		{ game:'sangoWar', name:'三國志大戰', hot:true },
+		{ game:'sgs', name:'三國殺陣面對決', hot:true },
+		{ game:'magic', name:'魔法風雲會', hot:false },
+		{ game:'battleSpirits', name:'戰魂', hot:true },
+		{ game:'crusade', name:'Crusade', hot:false },
+		{ game:'ws', name:'黑白雙翼', hot:false },
+		{ game:'dragonZ', name:'七龍珠TCG', hot:false },
+		{ game:'yugioh', name:'遊戲王', hot:false }
+	];
 	
 	function addVerToTitle(){
 		$('title').html( $('title').html() + version );
 	}
 	
 	module.version = version
+	module.ary_games = ary_games
 	module.message = message
 	module.messageForMe = messageForMe
 	module.defaultMessageHandle = defaultMessageHandle
-	module.showHotGame = showHotGame
+	//module.showHotGame = showHotGame
 	module.getMeta = getMeta
 	module.admin = admin;
 	module.beta = beta;
