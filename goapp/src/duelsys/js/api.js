482fc6340820a883d0418afd5c63f7ae3b3c9317
HanYu <hanyushome@gmail.com>
2016-03-02 09:53:41 +0800

    no message

--- goapp/src/duelsys/js/api.js
+++ goapp/src/duelsys/js/api.js
@@ -19,8 +19,7 @@ var api = api || {};
 	}
 	
 	module.talk = talk
-	module.createDuel = "建立%s比賽。期間從%s到%s"
-	module.deleteDuel = "刪除%s比賽"
+	module.createDuel = "建立%s比賽。期間從%s到%s"	//期間的格式為yyyy-Jan-dd
 	module.duelContext = "比賽本文"
 	module.addPeople = "%s要參加%s比賽"
 	module.winState = "確認%s比賽的%s選手和%s選手的比賽結果"
