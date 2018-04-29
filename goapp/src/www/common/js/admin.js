52f1a8730949a164090f35b1dc848302ecc4ca49
HanYu1983 <hanyushome@gmail.com>
2018-04-29 14:56:09 +0800

    no message

--- goapp/src/www/common/js/admin.js
+++ goapp/src/www/common/js/admin.js
@@ -2,7 +2,7 @@
 
 (function(module){
 	
-	var version = '2.1';
+	var version = '2.2';
 	var admin = ( leo.utils.getHash().admin != undefined && leo.utils.getHash().admin == 'nimda' );
 	var beta = ( leo.utils.getHash().beta != undefined && leo.utils.getHash().beta == '1' );
 	var cardbackCount = 64;
