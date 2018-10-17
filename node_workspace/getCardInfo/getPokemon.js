9a8768010b75d7d154d38de91d4ad2b653c916b1
HanYu1983 <hanyushome@gmail.com>
2018-10-17 12:01:30 +0800

    no message

--- node_workspace/getCardInfo/getPokemon.js
+++ node_workspace/getCardInfo/getPokemon.js
@@ -140,9 +140,14 @@ async function getPackage(pkgId){
 }
 
 async function getDetails(detailPageUrls){
-	//let cardDetailReq = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->\n\s+<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">[\s\S]+<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->\n\s+<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->\n\s+<!-- START: ILLUSTRATOR -->([\s\S]+?)<!-- ENDS: ILLUSTRATOR -->/
-	var cardDetailReq = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->\n\s+<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+[\s\S]+?<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">[\s\S]+<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->\n\s+<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->\n\s+<!-- START: ILLUSTRATOR -->([\s\S]+?)<!-- ENDS: ILLUSTRATOR -->/
-	//let cardDetailReq = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->([\s\S]+?)<!-- ENDS: POKEMON CARD BASIC DESCRIPTION -->[\s\S]+?<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->[\s\S]+?<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->/
+	//let cardDetailReg = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->\n\s+<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">[\s\S]+<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->\n\s+<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->\n\s+<!-- START: ILLUSTRATOR -->([\s\S]+?)<!-- ENDS: ILLUSTRATOR -->/
+	//var cardDetailReg = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->\n\s+<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+[\s\S]+?<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">[\s\S]+<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->\n\s+<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->\n\s+<!-- START: ILLUSTRATOR -->([\s\S]+?)<!-- ENDS: ILLUSTRATOR -->/
+	
+	
+	var cardDetailReg = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->([\s\S]+?)<!-- ENDS: POKEMON CARD BASIC DESCRIPTION -->[\s\S]+?<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->[\s\S]+?<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->/
+	var nameTypeReg = /<div class="card-description">\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>/
+	var basicReg = /<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+[\s\S]+?<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">/
+	var stageReg = /Evolves From:\n\s+<a.+>\n\s+(.+)\n\s+<\/a>/
 	
 	var output = []
 	for(var i in detailPageUrls){
@@ -150,21 +155,55 @@ async function getDetails(detailPageUrls){
 		console.log(i, "/", detailPageUrls.length, ":", cardPageUrl)
 		
 		var cardPage = await fetch(cardPageUrl)
-		var parseDetail = cardDetailReq.exec(cardPage)
+		var parseDetail = cardDetailReg.exec(cardPage)
 		
 		try{
-			//var [ignore, img, basic, abi, state] = parseDetail
-			//output.push([img, basic, abi, state])
+			var [ignore, img, basic, abi, state] = parseDetail
+			img = img.split("/")[img.split("/").length-1]
 			
-			var [ignore, img, name, type, hp, energy] = parseDetail
-			output.push([img, name, type, hp, energy])
-			/*
-			var [ignore, img, name, type, hp, energy, abi, state, ill] = parseDetail
-			output.push([img, name, type, hp, energy, abi, state, ill])
-			*/
+			var name = ""
+			var type = ""
+			var hp = -1
+			var energy = "*"
+			var evolveFrom = "*"
+			
+			try{
+				var nameTypeRow = nameTypeReg.exec(basic)
+				var [ignore, name_, type_] = nameTypeRow
+				name_ = name_.replace(/ +<img.+\/>/, "")
+				name_ = name_.replace(/<\/?em>/g, "")
+				
+				name = name_
+				type = type_
+				
+				if(type.indexOf("Basic") != -1){
+					var [ignore, name_, type_, hp_, energy_] = basicReg.exec(basic)
+					energy_ = energy_.replace(/icon-/, "")
+					
+					hp = hp_
+					energy = energy_
+				}
+				
+				if(type.indexOf("Stage") != -1){
+					var [ignore, evolveFrom_] = stageReg.exec(basic)
+					evolveFrom = evolveFrom_
+				}
+			}catch(e){
+				console.log(e)
+				console.log(img)
+				output.push([img, name, type, hp, energy, evolveFrom, basic, abi, state])
+				break
+			}
+			output.push([img, name, type, hp, energy, evolveFrom, basic, abi, state])
+			
+			//var [ignore, img, name, type, hp, energy] = parseDetail
+			//output.push([img, name, type, hp, energy])
+			
+			//var [ignore, img, name, type, hp, energy, abi, state, ill] = parseDetail
+			//output.push([img, name, type, hp, energy, abi, state, ill])
+			
 		}catch(e){
 			console.log(e)
-			console.log(parseDetail)
 		}
 	}
 	return output
@@ -173,16 +212,18 @@ async function getDetails(detailPageUrls){
 async function pokemonEn(outputPath){
 	var pkgs = await getPackages()
 	console.log(pkgs)
-	
+
 	var files = []
 	for(var i in pkgs){
 		var [pkgId, pkgName] = pkgs[i]
 		var links = await getPackage(pkgId)
 		var output = await getDetails(links)
+		// 記憶體不足,拆成二個部分
 		var tmpFileName = 'tmp/'+pkgName+".json"
 		await writeFile(tmpFileName, JSON.stringify(output, null, 2))
 		
 		files.push(tmpFileName)
+		break
 	}
 	
 	var output = []
