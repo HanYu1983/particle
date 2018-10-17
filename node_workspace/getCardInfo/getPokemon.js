20b21c538f7cda903bd019ae633ee74646139522
HanYu1983 <hanyushome@gmail.com>
2018-10-17 17:20:31 +0800

    no message

--- node_workspace/getCardInfo/getPokemon.js
+++ node_workspace/getCardInfo/getPokemon.js
@@ -345,7 +345,7 @@ async function getDetails(detailPageUrls){
 				// ignore
 			}
 			
-			img = img.split("/")[img.split("/").length-1]
+			var id = img.split("/")[img.split("/").length-1]
 			var ruleImgReg = /<img class=".+" alt="(.+)" \/>/g
 			var row = 0
 			while(row = ruleImgReg.exec(exRuleTxt)){
@@ -354,7 +354,8 @@ async function getDetails(detailPageUrls){
 			}
 			
 			output.push({
-				"id":img, 
+				"id":id,
+				"img":img,
 				"name":name, 
 				"type":type, 
 				"hp":hp, 
@@ -423,98 +424,23 @@ async function pokemonEn(outputPath){
 	await writeFile(outputPath, JSON.stringify(output, null, 2))
 }
 
-
-/*
-async function pokemonEn(outputPath){
-	let cardLinkReq = /<a href="(\/us\/pokemon-tcg\/pokemon-cards\/.+?\/)">/g
-	//let cardDetailReq = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->\n\s+<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">[\s\S]+<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->\n\s+<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->\n\s+<!-- START: ILLUSTRATOR -->([\s\S]+?)<!-- ENDS: ILLUSTRATOR -->/
-	let cardDetailReq = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->\n\s+<div class=.+>\n\s+<div.+>\n\s+<h1>(.+)<\/h1>\n\s+<\/div>\n\s+<div.+>\n\s+<div.+>\n\s+<h2>(.+)<\/h2>\n\s+[\s\S]+?<\/div>\n\s+<div.+>\n\s+<span class="card-hp"><span>HP<\/span>(.+)<\/span>\n\s+.+"energy (.+)">[\s\S]+<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->\n\s+<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->\n\s+<!-- START: ILLUSTRATOR -->([\s\S]+?)<!-- ENDS: ILLUSTRATOR -->/
-	//let cardDetailReq = /<section class="mosaic section card-detail">\n\s+<div.+>\n\s+<!-- START: MAIN CARD PICTURE -->\n\s+<div.+>\n\s+<img src="(.+)"\n\s+.+>[\s\S]+<!-- START: POKEMON CARD BASIC DESCRIPTION -->([\s\S]+?)<!-- ENDS: POKEMON CARD BASIC DESCRIPTION -->[\s\S]+?<!-- START: POKEMON CARD ABILITIES -->([\s\S]+?)<!-- ENDS: POKEMON CARD ABILITIES -->[\s\S]+?<!-- START: POKEMON CARD STATS AND EXPANSIONS -->([\s\S]+?)<!-- ENDS: POKEMON CARD STATS AND EXPANSIONS -->/
-	
-	//let pkgReq = /<li class=".+">\n\s+<input type="checkbox"\n\s+id="(.+)"\n\s+name=".+"\n\s+class=".+"\n\s+data-check-group=".+">\n\s+<label for=".+"\n\s+class=".+">\n\s+<i class="expansion"/g
-	let pkgNameReq = /<li class=".+">\n\s+<input type="checkbox"\n\s+id="(.+)"\n\s+name=".+"\n\s+class=".+"\n\s+data-check-group=".+">\n\s+<label for=".+"\n\s+class=".+">\n\s+<i class="expansion"[\s\S]+?<span>(.+)<\/span>/g
-	let totalPageReq = /<span >\d+ of (\d+)<\/span>/
-	
-	
-	let datas = []
-	let host = 'https://www.pokemon.com'
-	let path = '/us/pokemon-tcg/pokemon-cards/'
-	let querystring = '?cardName=&cardText=&evolvesFrom=&simpleSubmit=&format=unlimited&hitPointsMin=0&hitPointsMax=250&retreatCostMin=0&retreatCostMax=5&totalAttackCostMin=0&totalAttackCostMax=5&particularArtist=&sort=number&sort=number'
-	
-	let page = await fetch(host + path, true)
-	var row = 0
-	
-	var detailPageUrls = []
-	
-	while(row = pkgNameReq.exec(page)){
-		var [ignore, pkgId, pkg] = row
-		console.log(pkgId, pkg)
-		
-		var pkgPageUrl = host + path + '1' + querystring + '&'+pkgId+'=on'
-		console.log(pkgPageUrl)
-		var pkgPage = await fetch(pkgPageUrl)
-		var parse = totalPageReq.exec(pkgPage)
-		var [ignore, totalPage] = parse
-		console.log(totalPage)
-		
-		totalPage = parseInt(totalPage)
-		for(var i=1; i<=totalPage; ++i){
-			pkgPageUrl = host + path + i + querystring + '&'+pkgId+'=on'
-			console.log(pkgPageUrl)
-			pkgPage = await fetch(pkgPageUrl)
-			
-			var cardLinkRow = 0
-			while(cardLinkRow = cardLinkReq.exec(pkgPage)){
-				var [ignore, cardLink] = cardLinkRow
-				console.log(cardLink)
-				detailPageUrls.push(cardLink)
-			}
-		}
+async function genImgHtml(){
+	var pkgs = await getPackages()
+	var files = []
+	for(var i in pkgs){
+		var [pkgId, pkgName] = pkgs[i]
+		var tmpFileName = 'tmp/'+pkgName+".json"
+		var data = await readFile(tmpFileName)
+		data = JSON.parse(data)
+		data = data.map(c=>{
+			return "<img src=\""+c.img+"\">"
+		}).join('\n')
+		await writeFile('tmp/'+pkgName+"_img.html", data)
 	}
-	
-	
-	var output = []
-	for(var i in detailPageUrls){
-		var cardPageUrl = host + detailPageUrls[i]
-		console.log(i, "/", detailPageUrls.length, ":", cardPageUrl)
-		
-		var cardPage = await fetch(cardPageUrl)
-		var parseDetail = cardDetailReq.exec(cardPage)
-		
-		try{
-			//var [ignore, img, basic, abi, state] = parseDetail
-			//output.push([img, basic, abi, state])
-			
-
-			var [ignore, img, name, type, hp, energy, abi, state, ill] = parseDetail
-			output.push([img, name, type, hp, energy, abi, state, ill])
-		}catch(e){
-			console.log(e)
-			console.log(parseDetail)
-		}
-	}
-	
-	await writeFile(outputPath, JSON.stringify(output, null, 2))
 }
-*/
+
+
 (async function(){
 	await pokemonEn('build/pokemonEn.json')
-	
-	/*
-	var pkgs = await getPackages()
-	console.log(pkgs)
-	
-	var links = await getPackage(pkgs[0][0])
-	console.log(links)
-	
-	var output = await getDetails(links)
-	console.log(output)
-	*/
-	//await pokemonEn('build/pokemonEn.json')
-	
-	/*
-	await test('https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/sm-series/sm75/2/')
-	await test('https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/sm-series/sm75/3/')
-	await test('https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/sm-series/sm75/4/')
-	*/
+	await genImgHtml()
 })()
