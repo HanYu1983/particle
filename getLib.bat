9c057bcddb9951b1fe4f9fce74ce77a64de77daa
HanYu1983 <hanyushome@gmail.com>
2019-10-11 21:58:15 +0800

    no message

--- getLib.bat
+++ getLib.bat
@@ -1,4 +1,5 @@
 set GOPATH=D:\Documents\git\particle\goapp
-goapp.bat get google.golang.org/appengine
+goapp.bat get github.com/golang/protobuf/protoc-gen-go
+:: goapp.bat get google.golang.org/appengine
 :: goapp.bat get github.com/google/uuid
 :: goapp.bat get github.com/gorilla/mux
