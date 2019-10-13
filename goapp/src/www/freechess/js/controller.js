8d2744acd59b2e13087496e5a3e0118c1f560e8b
HanYu1983 <hanyushome@gmail.com>
2019-10-13 15:28:33 +0800

    no message

--- goapp/src/www/freechess/js/controller.js
+++ goapp/src/www/freechess/js/controller.js
@@ -283,6 +283,16 @@ var controller = controller || {};
             if (err) {
                 return;
             }
+            // TODO
+            /*api.restoreListening(ctx,
+                myId,
+                msg => {
+                    handleMsg(msg);
+                },
+                (isSuccess, info) => {
+                    console.log("target alive ", isSuccess, info)
+                })*/
+
             gameObj = refreshFourGame(ctx);
             smallAllTable();
 
