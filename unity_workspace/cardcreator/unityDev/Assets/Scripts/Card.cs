82fb4c739e365dfc592a54c912feaf82ec3828c6
HanYu1983 <hanyushome@gmail.com>
2021-01-25 00:57:27 +0800

    no message

--- unity_workspace/cardcreator/unityDev/Assets/Scripts/Card.cs
+++ unity_workspace/cardcreator/unityDev/Assets/Scripts/Card.cs
@@ -2,64 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Networking;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, ICardTemplate
 {
+    public bool runTest;
     public Image myImg;
+    Texture2D texture;
 
-    public Canvas canvas;
-
-    // Start is called before the first frame update
     void Start()
     {
-        yourMethod();
-
+        texture = new Texture2D(260, 390);
         CanvasScreenShot.OnPictureTaken += receivePNGScreenShot;
+        if (runTest)
+        {
+            StartCoroutine(PrintImage(Application.persistentDataPath + "/CanvasScreenShot.png", new string[] { "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQQ0G_OFWeKfnAAQ4Vg_KJfFH9Eyxl9R-Q67vVSdrinMbMzJcaA" }));
+        }
     }
 
     void receivePNGScreenShot(byte[] pngArray)
     {
-        //Debug.Log(GetComponent<RectTransform>().position);
-        Debug.Log("Picture taken");
-
+        Debug.Log("這裡很像不會呼叫到?");
         GetComponent<RectTransform>().SetPositionAndRotation(Vector3.zero, Quaternion.identity);
-
-        //Do Something With the Image (Save)
-
-       // string path = Application.persistentDataPath + "/CanvasScreenShot.png";
-       // System.IO.File.WriteAllBytes(path, pngArray);
-       // Debug.Log(path);
     }
 
-    public void yourMethod()
+    public IEnumerator CreateTexture(string url)
     {
-        StartCoroutine(setImage("https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQQ0G_OFWeKfnAAQ4Vg_KJfFH9Eyxl9R-Q67vVSdrinMbMzJcaA")); //balanced parens CAS
-    }
-
-    IEnumerator setImage(string url)
-    {
-        Texture2D texture = new Texture2D(260, 390);
-
-
         WWW www = new WWW(url);
         yield return www;
 
-        // calling this function with StartCoroutine solves the problem
-        Debug.Log("Why on earh is this never called?");
-
+        Texture2D texture = new Texture2D(260, 390);
         www.LoadImageIntoTexture(texture);
         www.Dispose();
         www = null;
+    }
+
+    
+
+    public IEnumerator PrintImage(string output, string[] info)
+    {
+        var url = info[0];
+
+        // ========= get texture ============= //
+        var cachedFileName = UnityWebRequest.EscapeURL(url);
+        var cachedPath = Application.persistentDataPath + "/" + cachedFileName;
+        if (File.Exists(cachedPath))
+        {
+            Debug.Log("loadCachedFile: "+cachedPath);
+            var loadBytes = File.ReadAllBytes(cachedPath);
+            texture.LoadImage(loadBytes);
+        }
+        else
+        {
+            WWW www = new WWW(url);
+            yield return www;
+
+            www.LoadImageIntoTexture(texture);
+            www.Dispose();
+            www = null;
+
+            byte[] bytes = texture.EncodeToPNG();
+            File.WriteAllBytes(cachedPath, bytes);
+            Debug.Log("WriteCachedFile: " + cachedPath);
+        }
+        // ================================= //
 
         Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
         myImg.sprite = s;
+        
+        ScreenCapture.CaptureScreenshot(output);
 
-        //GetComponent<CanvasScreenShot>().takeScreenShot(canvas, SCREENSHOT_TYPE.IMAGE_AND_TEXT, false);
-
-        string path = Application.persistentDataPath + "/CanvasScreenShot.png";
-        ScreenCapture.CaptureScreenshot(path);
-
-        Debug.Log(path);
-
+        Debug.Log(output);
     }
 }
---
+++ unity_workspace/cardcreator/unityDev/Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICardTemplate
+{
+    IEnumerator PrintImage(string output, string[] info);
+}
+
+public class Controller : MonoBehaviour
+{
+    public Card cardTemplate;
+
+    void Start()
+    {
+        StartCoroutine(StartPrint());
+    }
+
+    IEnumerator StartPrint()
+    {
+        yield return null;
+        yield return cardTemplate.PrintImage(Application.persistentDataPath + "/CanvasScreenShot0.png", new string[] { "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQQ0G_OFWeKfnAAQ4Vg_KJfFH9Eyxl9R-Q67vVSdrinMbMzJcaA" });
+        yield return cardTemplate.PrintImage(Application.persistentDataPath + "/CanvasScreenShot1.png", new string[] { "http://3.bp.blogspot.com/-5yxG58BC0Ps/Vjf-LHWd2zI/AAAAAAABNE8/plE-kaxx7f8/s1600/26813-boeing-f-15se-silent-eagle-1920x1200-aircraft-wallpaper.jpg" });
+
+    }
+}
