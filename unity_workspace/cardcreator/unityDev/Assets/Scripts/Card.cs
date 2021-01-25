767a5db269afd9d4cc13d715a8a35668b0770be4
余漢平 <yuhanping@wangyuan.com>
2021-01-25 11:37:42 +0800

    no message

--- unity_workspace/cardcreator/unityDev/Assets/Scripts/Card.cs
+++ unity_workspace/cardcreator/unityDev/Assets/Scripts/Card.cs
@@ -8,23 +8,23 @@ using UnityEngine.Networking;
 public class Card : MonoBehaviour, ICardTemplate
 {
     public bool runTest;
-    public Image myImg;
+
+    public GameObject CardType;
+    public GameObject[] CardTypeIcons;
+
+    public GameObject[] CardFronts;
+
+
+    public Image CardImage;
     Texture2D texture;
 
     void Start()
     {
         texture = new Texture2D(260, 390);
-        CanvasScreenShot.OnPictureTaken += receivePNGScreenShot;
         if (runTest)
         {
             StartCoroutine(PrintImage(Application.persistentDataPath + "/CanvasScreenShot.png", new string[] { "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQQ0G_OFWeKfnAAQ4Vg_KJfFH9Eyxl9R-Q67vVSdrinMbMzJcaA" }));
         }
-    }
-
-    void receivePNGScreenShot(byte[] pngArray)
-    {
-        Debug.Log("這裡很像不會呼叫到?");
-        GetComponent<RectTransform>().SetPositionAndRotation(Vector3.zero, Quaternion.identity);
     }
     
     public IEnumerator PrintImage(string output, string[] info)
@@ -55,9 +55,8 @@ public class Card : MonoBehaviour, ICardTemplate
         }
         // ================================= //
         
-        Sprite s = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
-        myImg.sprite = s;
-        
+        CardImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
         ScreenCapture.CaptureScreenshot(output);
 
         Debug.Log(output);
