99e30656192ebeb525f508cc0400c705fac4c22a
HanYu1983 <hanyushome@gmail.com>
2022-03-23 11:21:26 +0800

    add fighter

--- goapp/src/www/common/js/fighter.js
+++ goapp/src/www/common/js/fighter.js
@@ -1,36 +1,49 @@
 var fighter = fighter || {};
 
-(function( module ){
-	
-	function load( path, cb ){
-		$.ajax({
-			url: path,
-			dataType:'text',
-			success:function(data){
-				// skip first line
-				var [_, ...lines] = data.split('\n')
-				var jsons = lines.map(line=>line.split('\t')).map(info=>{
-					return {
-						id: info[0],
-						cid: info[0],
-						level: parseInt(info[1]),
-						cname: info[2],
-						weight: parseInt(info[3]),
-						ctype: info[4],
-						atk: parseInt(info[5]),
-						abi1: info[6],
-						abi2: info[7],
-						abi3: info[8]
-					}
-				})
-				cb(null, jsons)
-			},
-			error: function(xhr, res, err){
-				cb(err)
-			}
-		})
-	}
+(function (module) {
+  function load(path, cb) {
+    $.ajax({
+      url: path,
+      dataType: "text",
+      success: function (data) {
+        // skip first line
+        var [_, ...lines] = data.split("\n");
+        var jsons = lines
+          .map((line) => line.split("\t"))
+          .map((row) => {
+            const id = row[0];
+            const title = row[1];
+            const weight = row[2];
+            const attackType = row[3];
+            const outsidePower = row[4];
+            const insidePower = row[5];
+            const skillType = row[6];
+            const skillCost = row[7];
+            const skillName = row[8];
+            const skillMana = row[9];
+            const skillText = row[10];
+            return {
+              id: id,
+              cid: id,
+              title: title,
+              skillType: skillType,
+              weight: weight,
+              outsidePower: outsidePower,
+              insidePower: insidePower,
+              attackType: attackType,
+              skillCost: skillCost,
+              skillName: skillName,
+              skillText: skillText,
+              skillMana: skillMana,
+            };
+          });
+        cb(null, jsons);
+      },
+      error: function (xhr, res, err) {
+        cb(err);
+      },
+    });
+  }
 
-	module.load = load
-	
-}) ( fighter )
+  module.load = load;
+})(fighter);
