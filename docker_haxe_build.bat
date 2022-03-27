2fb401bcc85360de18d6d381a3a60a8ef8c50b49
HanYu1983 <hanyushome@gmail.com>
2022-03-27 18:27:08 +0800

    add haxe docker build

--- docker_haxe_build.bat
+++ docker_haxe_build.bat
@@ -1,1 +1,1 @@
-docker run --rm -ti -v d:\dev\particle\haxe_workspace:/haxe_workspace haxe:4.2.4 bash 
+docker run --rm -ti -v d:\dev\particle\haxe_workspace:/haxe_workspace -v d:\dev\particle\goapp:/goapp -w /haxe_workspace/card/gameplay haxe:4.2.4 haxe build.hxml
